de9b3357eb1a3f319f5d805b9f8a31da8afd095a
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-03-06 16:04:44 +0100

    created a function to split the data into features and label

--- 002_Cognizant_Artificial_Intelligence/4_Machine_Learning_Production/module_starter.py
+++ 002_Cognizant_Artificial_Intelligence/4_Machine_Learning_Production/module_starter.py
@@ -52,4 +52,30 @@ def load_data(path: str = "/path/to/csv/"):
     df.drop(columns=["Unnamed: 0"], inplace=True, error='ignore')
     return df
 
+# Create target varable and predictor variables
+def create_target_and_predictors(
+        data: pd.DataFrame = None,
+        target: str = "estimated_stock_pct"
+):
+    """
+    This function takes in a pandas DataFrame and splits the columns
+    into a target column and a set of predictor variables, i.e X and y.
+    These tow splits of the data will be used to train a supervided
+    machine learning model
+
+    :param      data: pd.DataFrame, dataframe containing data for the model
+
+    :param      target: str (optional), target variable that you want to predict
+
+    :return     X: pd.DataFrame
+                y: pd.Series
+    """
+
+    # Check to see if the target variable is present in the data
+    if target not in data.columns:
+        raise Exception(f"Targe: {target} is not present in the data")
+    
+    X = data.drop(columns=[target])
+    y = data[target]
+    return X, y
 
